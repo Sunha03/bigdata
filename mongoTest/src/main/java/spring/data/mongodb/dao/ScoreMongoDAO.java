de0acb3fd05b502c7f00b51f675c5a71f886fb77
Sunha03 <ahnus123@gmail.com>
2020-03-18 17:37:07 +0900

    MongoTest project - Paging, STS + MongoDB

--- mongoTest/src/main/java/spring/data/mongodb/dao/ScoreMongoDAO.java
+++ mongoTest/src/main/java/spring/data/mongodb/dao/ScoreMongoDAO.java
@@ -5,10 +5,12 @@ import java.util.List;
 import spring.data.mongodb.dto.ScoreDTO;
 
 public interface ScoreMongoDAO {
+	public List<ScoreDTO> findIntCriteria(String key,int value);
 	public List<ScoreDTO> findCriteria(String key,String value);
 	public ScoreDTO findById(String key,String value);
 	public void insertDocument(ScoreDTO doc);
 	public void insertAllDocument(List<ScoreDTO> docs);
 	public void update(ScoreDTO document);
-	public void test1();
+	public List<ScoreDTO> findAll();
+	public List<ScoreDTO> findAll(int pageNo);
 }
--- mongoTest/src/main/java/spring/data/mongodb/dto/ScoreDTO.java
+++ mongoTest/src/main/java/spring/data/mongodb/dto/ScoreDTO.java
@@ -1,39 +1,43 @@
 package spring.data.mongodb.dto;
+//mongodb Collection에서 하나의 document와 매핑될 클래스
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="score")
 public class ScoreDTO {
+	@Id
 	String _id;
 	String id;
 	String name;
 	String dept;
 	String addr;
-	int java;
+	int java; 
 	int servlet;
-	int bonus;
-	int spring;
-	public ScoreDTO(){
+	public ScoreDTO() {
 		
 	}
-	public ScoreDTO(String _id, String id, String name, String dept, String addr, int java, int servlet, int bonus,
-			int spring) {
+	
+	@Override
+	public String toString() {
+		return "ScoreDTO [id=" + id + ", name=" + name + ", dept=" + dept + ", addr=" + addr + ", java=" + java
+				+ ", servlet=" + servlet + "]";
+	}
+
+
+
+	public ScoreDTO(String id, String name, String dept, String addr, int java, int servlet) {
 		super();
-		this._id = _id;
 		this.id = id;
 		this.name = name;
 		this.dept = dept;
 		this.addr = addr;
 		this.java = java;
 		this.servlet = servlet;
-		this.bonus = bonus;
-		this.spring = spring;
 	}
-	public String get_id() {
-		return _id;
-	}
-	public void set_id(String _id) {
-		this._id = _id;
-	}
+
+
+
 	public String getId() {
 		return id;
 	}
@@ -70,22 +74,7 @@ public class ScoreDTO {
 	public void setServlet(int servlet) {
 		this.servlet = servlet;
 	}
-	public int getBonus() {
-		return bonus;
-	}
-	public void setBonus(int bonus) {
-		this.bonus = bonus;
-	}
-	public int getSpring() {
-		return spring;
-	}
-	public void setSpring(int spring) {
-		this.spring = spring;
-	}
-	@Override
-	public String toString() {
-		return "ScoreDTO [_id=" + _id + ", id=" + id + ", name=" + name + ", dept=" + dept + ", addr=" + addr
-				+ ", java=" + java + ", servlet=" + servlet + ", bonus=" + bonus + ", spring=" + spring + "]";
-	}
+	
+	
 	
 }
